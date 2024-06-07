2ac626366b5700102e57d7296102ef4d36f0da7c
HarshvardhanPandey2003 <harshvardhanpandey2003@gmail.com>
2024-06-07 23:38:37 +0530

    Adding Delete and Show Function

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends, Response,status
 from . import schemas,models
 from .database import engine,SessionLocal
 from sqlalchemy.orm import Session 
@@ -13,12 +13,35 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/blog")
+@app.post("/blog",status_code=201)
 def create(request: schemas.Blog,db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
-def create(request:schemas.Blog):
-    return {"Title":schemas.Blog.title, "body":schemas.Blog.body}
+
+@app.get("/blog")
+def all(db:Session = Depends(get_db)):
+    return db.query(models.Blog).all()
+
+@app.get("/blog/{id}",status_code=200)
+def show(id,response:Response, db:Session = Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'detail': f'Blog with the id {id} not found'}
+    return blog
+
+@app.delete("/blog/{id}", status_code=204)
+def delete_blog(id: int,response:Response, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"detail": f"Blog with the id {id} not found"}
+
+    db.delete(blog)
+    db.commit()
+
+    remaining_blogs = db.query(models.Blog).all()
+    return {"message": f"Blog with id {id} has been deleted", "remaining_blogs": remaining_blogs}
